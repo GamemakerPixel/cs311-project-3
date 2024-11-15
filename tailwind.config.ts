e80d07bda8c5c360351a97e99c2f5e98427131f3
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-11-15 11:39:07 -0500

    Clear unnessesary initial files and create tailwind color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,17 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#0e273c",
+        secondary: "#e4dbca",
+        accent: "#f82866",
+        text: "#efece6",
+        itext: "#051420",
       },
     },
   },
